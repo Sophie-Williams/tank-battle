351f2e3ba89414e409a552848e1433c7fa78ff82
VincentPT <minhpta@outlook.com>
2018-07-26 21:41:12 +0700

    add fire function

--- src/GameControllers/ScriptedPlayer/PlayerSciptingLibrary.cpp
+++ src/GameControllers/ScriptedPlayer/PlayerSciptingLibrary.cpp
@@ -88,6 +88,7 @@ ConstOperandBase* createTurningConsant(TurningDir cosnt_val) {
 void PlayerContextSciptingLibrary::loadContextFunctions(ScriptCompiler* scriptCompiler) {
 	FunctionRegisterHelper helper(scriptCompiler);
 	REGIST_CONTEXT_FUNCTION0(helper, freeze, void);
+	REGIST_CONTEXT_FUNCTION0(helper, fire, void);
 	REGIST_CONTEXT_FUNCTION1(helper, setMove, void, MovingDir);
 	REGIST_CONTEXT_FUNCTION1(helper, setTurn, void, TurningDir);
 	REGIST_CONTEXT_FUNCTION1(helper, setGunTurn, void, TurningDir);
