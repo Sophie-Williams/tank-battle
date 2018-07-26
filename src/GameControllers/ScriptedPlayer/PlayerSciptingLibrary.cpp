f02b7c36b8904e041d5ce43af53a6194665c679a
Minh Pham <minh@sapientific.com>
2018-07-26 08:46:26 +0700

    add some context function for script.

--- src/GameControllers/ScriptedPlayer/PlayerSciptingLibrary.cpp
+++ src/GameControllers/ScriptedPlayer/PlayerSciptingLibrary.cpp
@@ -52,94 +52,153 @@ public:
 
 #define REGIST_CONTEXT_FUNCTION1(helper, func, returnType , ...) helper.registFunction(#func, #__VA_ARGS__, new BasicFunctionFactory<SIZE_OF_ARGS(__VA_ARGS__)>(EXP_UNIT_ID_USER_FUNC, FUNCTION_PRIORITY_USER_FUNCTION, #returnType, new MFunction2<returnType, PlayerContextSciptingLibrary, __VA_ARGS__>(this, &PlayerContextSciptingLibrary::func), helper.getSriptCompiler()))
 
+namespace ScriptingLib {
+	///////////////////////////////////////////////////////////////////////////////////////////
+	ConstOperandBase* createMovingConsant(MovingDir cosnt_val) {
+		return new CConstOperand<MovingDir>(cosnt_val, "MovingDir");
+	}
 
-void PlayerContextSciptingLibrary::setMove(MovingDir dir) {
-	_commandBuilder.move(dir);
-}
+	ConstOperandBase* createTurningConsant(TurningDir cosnt_val) {
+		return new CConstOperand<TurningDir>(cosnt_val, "TurningDir");
+	}
 
-void PlayerContextSciptingLibrary::setTurn(TurningDir dir) {
-	_commandBuilder.turn(dir);
-}
+	ConstOperandBase* createRotatingConsant(RotatingDir cosnt_val) {
+		return new CConstOperand<RotatingDir>(cosnt_val, "RotatingDir");
+	}
 
-void PlayerContextSciptingLibrary::setGunTurn(TurningDir dir) {
-	_commandBuilder.spinGun(dir);
-}
+	///////////////////////////////////////////////////////////////////////////////////////////
+	void PlayerContextSciptingLibrary::move(MovingDir dir) {
+		_commandBuilder.move(dir);
+	}
 
-void PlayerContextSciptingLibrary::freeze() {
-	_commandBuilder.freeze();
-}
+	void PlayerContextSciptingLibrary::turn(TurningDir dir) {
+		_commandBuilder.turn(dir);
+	}
 
-void PlayerContextSciptingLibrary::fire() {
-	_commandBuilder.fire();
-}
+	void PlayerContextSciptingLibrary::rotateGun(RotatingDir dir) {
+		_commandBuilder.spinGun(dir);
+	}
 
-void PlayerContextSciptingLibrary::keepPreviousState() {
-	_tankOperations = TANK_NULL_OPERATION;
-}
+	void PlayerContextSciptingLibrary::freeze() {
+		_commandBuilder.freeze();
+	}
 
-ConstOperandBase* createMovingConsant(MovingDir cosnt_val) {
-	return new CConstOperand<MovingDir>(cosnt_val, "MovingDir");
-}
+	void PlayerContextSciptingLibrary::fire() {
+		_commandBuilder.fire();
+	}
 
-ConstOperandBase* createTurningConsant(TurningDir cosnt_val) {
-	return new CConstOperand<TurningDir>(cosnt_val, "TurningDir");
-}
+	void PlayerContextSciptingLibrary::keepPreviousState() {
+		_tankOperations = TANK_NULL_OPERATION;
+	}
 
-void PlayerContextSciptingLibrary::loadContextFunctions(ScriptCompiler* scriptCompiler) {
-	FunctionRegisterHelper helper(scriptCompiler);
-	REGIST_CONTEXT_FUNCTION0(helper, freeze, void);
-	REGIST_CONTEXT_FUNCTION1(helper, setMove, void, MovingDir);
-	REGIST_CONTEXT_FUNCTION1(helper, setTurn, void, TurningDir);
-	REGIST_CONTEXT_FUNCTION1(helper, setGunTurn, void, TurningDir);
-}
+	///////////////////////////////////////////////////////////////////////////////////////////
+	Ray PlayerContextSciptingLibrary::gun() {
+		return _temporaryPlayerContex->getMyGun();
+	}
 
-PlayerContextSciptingLibrary::PlayerContextSciptingLibrary() : _commandBuilder(_tankOperations) {
-	resetCommand();
-}
+	float PlayerContextSciptingLibrary::health() {
+		return _temporaryPlayerContex->getMyHealth();
+	}
 
-void PlayerContextSciptingLibrary::resetCommand() {
-	_tankOperations = TANK_NULL_OPERATION;
-}
+	GeometryInfo PlayerContextSciptingLibrary::geometry() {
+		return _temporaryPlayerContex->getMyGeometry();
+	}
 
-TankOperations PlayerContextSciptingLibrary::getOperations() const {
-	return _tankOperations;
-}
+	float PlayerContextSciptingLibrary::movingSpeed() {
+		return _temporaryPlayerContex->getMovingSpeed();
+	}
 
-void PlayerContextSciptingLibrary::loadLibrary(ScriptCompiler* scriptCompiler) {
-	
-	// register types
-	auto type = scriptCompiler->registType("MovingDir");
-	scriptCompiler->setTypeSize(type, 1);
+	float PlayerContextSciptingLibrary::turningSpeed() {
+		return _temporaryPlayerContex->getTurningSpeed();
+	}
 
-	type = scriptCompiler->registType("TurningDir");
-	scriptCompiler->setTypeSize(type, 1);
+	TankOperations PlayerContextSciptingLibrary::currentOperations() {
+		return _temporaryPlayerContex->getCurrentOperations();
+	}
 
-	// register contants
-	// moving contants
-	auto NO_MOVE = make_shared<CdeclFunction<ConstOperandBase*, MovingDir>>(createMovingConsant);
-	NO_MOVE->pushParam((void*)0);
-	scriptCompiler->setConstantMap("NO_MOVE", NO_MOVE);
+	bool PlayerContextSciptingLibrary::isAlly(GameObjectId id) {
+		return _temporaryPlayerContex->isAlly(id);
+	}
 
-	auto MOVE_FORWARD = make_shared<CdeclFunction<ConstOperandBase*, MovingDir>>(createMovingConsant);
-	MOVE_FORWARD->pushParam((void*)1);
-	scriptCompiler->setConstantMap("MOVE_FORWARD", MOVE_FORWARD);
+	bool PlayerContextSciptingLibrary::isEnemy(GameObjectId id) {
+		return _temporaryPlayerContex->isEnemy(id);
+	}
 
-	auto MOVE_BACKWARD = make_shared<CdeclFunction<ConstOperandBase*, MovingDir>>(createMovingConsant);
-	MOVE_BACKWARD->pushParam((void*)-1);
-	scriptCompiler->setConstantMap("MOVE_BACKWARD", MOVE_BACKWARD);
-	// turning consants
-	auto NO_TURN = make_shared<CdeclFunction<ConstOperandBase*, TurningDir>>(createTurningConsant);
-	NO_TURN->pushParam((void*)0);
-	scriptCompiler->setConstantMap("NO_TURN", NO_TURN);
+	///////////////////////////////////////////////////////////////////////////////////////////
+	void PlayerContextSciptingLibrary::loadContextFunctions(ScriptCompiler* scriptCompiler) {
+		FunctionRegisterHelper helper(scriptCompiler);
+		REGIST_CONTEXT_FUNCTION0(helper, freeze, void);
+		REGIST_CONTEXT_FUNCTION1(helper, move, void, MovingDir);
+		REGIST_CONTEXT_FUNCTION1(helper, turn, void, TurningDir);
+		REGIST_CONTEXT_FUNCTION1(helper, rotateGun, void, RotatingDir);
+	}
 
-	auto TURN_LEFT = make_shared<CdeclFunction<ConstOperandBase*, TurningDir>>(createTurningConsant);
-	TURN_LEFT->pushParam((void*)1);
-	scriptCompiler->setConstantMap("TURN_LEFT", TURN_LEFT);
+	PlayerContextSciptingLibrary::PlayerContextSciptingLibrary() : _commandBuilder(_tankOperations) {
+		resetCommand();
+	}
 
-	auto TURN_RIGHT = make_shared<CdeclFunction<ConstOperandBase*, TurningDir>>(createTurningConsant);
-	TURN_RIGHT->pushParam((void*)-1);
-	scriptCompiler->setConstantMap("TURN_RIGHT", TURN_RIGHT);
+	void PlayerContextSciptingLibrary::resetCommand() {
+		_tankOperations = TANK_NULL_OPERATION;
+	}
 
-	// register global functions
-	loadContextFunctions(scriptCompiler);
+	TankOperations PlayerContextSciptingLibrary::getOperations() const {
+		return _tankOperations;
+	}
+
+	void PlayerContextSciptingLibrary::loadLibrary(ScriptCompiler* scriptCompiler) {
+
+		// register types
+		auto type = scriptCompiler->registType("MovingDir");
+		scriptCompiler->setTypeSize(type, 1);
+
+		type = scriptCompiler->registType("TurningDir");
+		scriptCompiler->setTypeSize(type, 1);
+
+		type = scriptCompiler->registType("RotatingDir");
+		scriptCompiler->setTypeSize(type, 1);
+
+
+
+		// register contants
+		// moving contants
+		auto NO_MOVE = make_shared<CdeclFunction<ConstOperandBase*, MovingDir>>(createMovingConsant);
+		NO_MOVE->pushParam((void*)0);
+		scriptCompiler->setConstantMap("NO_MOVE", NO_MOVE);
+
+		auto MOVE_FORWARD = make_shared<CdeclFunction<ConstOperandBase*, MovingDir>>(createMovingConsant);
+		MOVE_FORWARD->pushParam((void*)1);
+		scriptCompiler->setConstantMap("MOVE_FORWARD", MOVE_FORWARD);
+
+		auto MOVE_BACKWARD = make_shared<CdeclFunction<ConstOperandBase*, MovingDir>>(createMovingConsant);
+		MOVE_BACKWARD->pushParam((void*)-1);
+		scriptCompiler->setConstantMap("MOVE_BACKWARD", MOVE_BACKWARD);
+		// turning consants
+		auto NO_TURN = make_shared<CdeclFunction<ConstOperandBase*, TurningDir>>(createTurningConsant);
+		NO_TURN->pushParam((void*)0);
+		scriptCompiler->setConstantMap("NO_TURN", NO_TURN);
+
+		auto TURN_LEFT = make_shared<CdeclFunction<ConstOperandBase*, TurningDir>>(createTurningConsant);
+		TURN_LEFT->pushParam((void*)1);
+		scriptCompiler->setConstantMap("TURN_LEFT", TURN_LEFT);
+
+		auto TURN_RIGHT = make_shared<CdeclFunction<ConstOperandBase*, TurningDir>>(createTurningConsant);
+		TURN_RIGHT->pushParam((void*)-1);
+		scriptCompiler->setConstantMap("TURN_RIGHT", TURN_RIGHT);
+
+		// turning consants
+		auto NO_ROTATE = make_shared<CdeclFunction<ConstOperandBase*, RotatingDir>>(createRotatingConsant);
+		NO_ROTATE->pushParam((void*)0);
+		scriptCompiler->setConstantMap("NO_ROTATE", NO_ROTATE);
+
+		auto ROTATE_LEFT = make_shared<CdeclFunction<ConstOperandBase*, RotatingDir>>(createRotatingConsant);
+		ROTATE_LEFT->pushParam((void*)1);
+		scriptCompiler->setConstantMap("ROTATE_LEFT", ROTATE_LEFT);
+
+		auto ROTATE_RIGHT = make_shared<CdeclFunction<ConstOperandBase*, RotatingDir>>(createRotatingConsant);
+		ROTATE_RIGHT->pushParam((void*)-1);
+		scriptCompiler->setConstantMap("ROTATE_RIGHT", ROTATE_RIGHT);
+
+		// register global functions
+		loadContextFunctions(scriptCompiler);
+	}
 }
