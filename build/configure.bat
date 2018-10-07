5ebfd080a9b82c2b13a74071a603e23daa9c3545
Minh Pham <minhpta@outlook.com>
2018-10-07 15:36:29 +0700

    - update configure.bat

--- build/configure.bat
+++ build/configure.bat
@@ -5,8 +5,7 @@ SET CURR_DIR=%cd%
 SET THIS_SCRIPT_PATH=%~dp0
 cd %THIS_SCRIPT_PATH%
 
-cmake -G "Visual Studio 14 2015 Win64" -T v140 ../src ^
-        -DCINDER_INCLUDE_DIR=C:/Users/minh/Documents/working/Cinder/include -DCINDER_LIB_DIR=C:/Users/minh/Documents/working/Cinder/lib/msw/x64
+cmake -DCMAKE_GENERATOR_PLATFORM=x64 ../src
 cd %CURR_DIR%
 
 endlocal
